dd9415179fbb22755a802a2f6e3d37c3815b51a5
Shawn Snyder <shawnsnyder@Shawns-MBP.grandenetworks.net>
2018-02-13 15:08:07 -0600

    starting up

---
+++ scaffold/scaffold/Application/AppDependencies.swift
@@ -0,0 +1,43 @@
+//
+//  AppDependencies.swift
+//  base_project_tests
+//
+//  Created by Shawn Snyder on 2/13/18.
+//  Copyright © 2018 Shawn Snyder. All rights reserved.
+//
+
+import Foundation
+
+
+struct AppDependencies{
+    
+}
+
+
+extension AppDependencies {
+    /*
+     fileprivate static var backend : Go24MoyaBackend  {
+     
+     #if DEBUG
+     let actitityPlugin = NetworkActivityPlugin(networkActivityClosure:{
+     change, target in
+     })
+     let plugins : [PluginType] = [actitityPlugin, NetworkLoggerPlugin()]
+     #else
+     let plugins : [PluginType] = []
+     #endif
+     
+     
+     let provider = MoyaProvider<Go24Service>(requestClosure: Go24RequestConfiguration.requestClosure,
+     plugins:plugins)
+     
+     return Go24MoyaBackend(provider:provider)
+     }
+     */
+    /*
+     func resolve() -> BackendAPI {
+     return AppDependencies.backend
+     }
+     */
+}
+
